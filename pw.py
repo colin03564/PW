218654c781c4d5b7cea9b22cd684b9caa5d85c20
Colin Sung <colin03564@gmail.com>
2022-05-29 12:07:56 +0800

    2nd version

--- pw.py
+++ pw.py
@@ -1,12 +1,13 @@
 password = 'a123456'
-i = 3 
-while True:
+i = 3 #剩餘次數 
+while i > 0:
+    i = i - 1
     pw = input('請輸入密碼: ')
     if pw == password:
         print('登入成功!')
         break
     else:
-        i = i - 1
-        print('密碼錯誤! 還有', i, '次機會!')
         if i == 0:
-        	break
+            print('密碼錯誤,帳號鎖定')
+        else:
+            print('密碼錯誤! 還有', i, '次機會!')
